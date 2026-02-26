406b797c7219540596298e732dc8d3ff59a2b55f
Ganesh Paudel <99114439+Ganesh-Paudel@users.noreply.github.com>
2026-02-26 17:19:34 -0500

    Updated the Lexical analysis file.
    UPdated the UI. Added the check for variable names beginning with number. And optimized the code by removing the repeating statements. Also added the file location to be provided by user.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,19 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) throws IOException {
-        UI.printHeader("Tokens", "Lexemes");
-        LexicalAnalysis analyze = new LexicalAnalysis(new File("C:\\Users\\paude\\Desktop\\Projects\\front.txt"));
+
+        String pathToFile;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the path to the file: (Absolute path is appreciated) : ");
+        pathToFile = input.nextLine();
+        UI.printHeader();
+        LexicalAnalysis analyze = new LexicalAnalysis(new File(pathToFile));
+        UI.printFooter();
     }
 
 }
